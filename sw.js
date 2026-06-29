e2bf558159613bb690d070520b1b5b63ade807dd
linkhinojosa-cmyk <linkhinojosa@gmail.com>
2026-06-28 22:34:57 -0400

    Initial deploy

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "pokequest-v13";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://cdn.tailwindcss.com",
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok && e.request.method === "GET") {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy).catch(()=>{}));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
